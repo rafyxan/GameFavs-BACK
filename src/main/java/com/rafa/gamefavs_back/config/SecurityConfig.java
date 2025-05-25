f508a610d07e2f2ab9e46e6b2c6d413bff042064
rafyxan <148994841+rafyxan@users.noreply.github.com>
2025-05-25 13:10:11 +0200

    v2

--- src/main/java/com/rafa/gamefavs_back/config/SecurityConfig.java
+++ src/main/java/com/rafa/gamefavs_back/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+
                 .csrf(csrf -> csrf.disable()) // Disable CSRF
                 .cors(cors -> cors.disable()) // Disable CORS (or configure it if needed)
                 .authorizeHttpRequests(auth -> auth
